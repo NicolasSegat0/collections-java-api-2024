a48ac5858267d0814d7310c084ba086dcfd33c6a
Nicolas Navas Segato <ninavassegato@gmail.com>
2024-05-21 18:01:48 -0300

    Resolvendo exercícos do set
    Signed-off-by: Nicolas Navas Segato <ninavassegato@gmail.com>

---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,62 @@
+package main.java.set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if (c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("Nicolas", 123456);
+        agendaContatos.adicionarContato("Nicolas", 0);
+        agendaContatos.adicionarContato("Nicolas Navas", 11111);
+        agendaContatos.adicionarContato("Nicolas Segato", 67890);
+        agendaContatos.adicionarContato("Nicolas Nava Segato", 111111);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Nicolas"));
+        System.out.println("Contato atualizado" + agendaContatos.atualizarNumeroContato("Nicolas N S", 55555));
+
+    }
+
+}
